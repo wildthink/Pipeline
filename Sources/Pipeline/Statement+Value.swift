5e735751ef52d78d665709c9049c1a9237568033
Stephen F. Booth <me@sbooth.org>
2022-02-16 22:22:36 -0600

    Improve Database.Value

---
+++ Sources/Pipeline/Statement+Value.swift
@@ -0,0 +1,111 @@
+//
+// Copyright © 2021 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/Pipeline
+// MIT license
+//
+
+import Foundation
+import CSQLite
+
+extension Database.Statement {
+	/// Binds `value` to the SQL parameter at `index`.
+	///
+	/// - note: Parameter indexes are 1-based.  The leftmost parameter in a statement has index 1.
+	///
+	/// - parameter value: The desired value of the SQL parameter.
+	/// - parameter index: The index of the SQL parameter to bind.
+	///
+	/// - throws: An error if `value` couldn't be bound.
+	public func bind(value: Database.Value, toParameter index: Int) throws {
+		switch value {
+		case .integer(let i):
+			guard sqlite3_bind_int64(preparedStatement, Int32(index), i) == SQLITE_OK else {
+				throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
+			}
+		case .real(let r):
+			guard sqlite3_bind_double(preparedStatement, Int32(index), r) == SQLITE_OK else {
+				throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
+			}
+		case .text(let t):
+			try t.withCString {
+				guard sqlite3_bind_text(preparedStatement, Int32(index), $0, -1, SQLiteTransientStorage) == SQLITE_OK else {
+					throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
+				}
+			}
+		case .blob(let b):
+			try b.withUnsafeBytes {
+				guard sqlite3_bind_blob64(preparedStatement, Int32(index), $0.baseAddress, sqlite3_uint64(b.count), SQLiteTransientStorage) == SQLITE_OK else {
+					throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
+				}
+			}
+		case .null:
+			guard sqlite3_bind_null(preparedStatement, Int32(index)) == SQLITE_OK else {
+				throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
+			}
+		}
+	}
+
+	/// Binds `value` to the SQL parameter `name`.
+	///
+	/// - parameter value: The desired value of the SQL parameter.
+	/// - parameter name: The name of the SQL parameter to bind.
+	///
+	/// - throws: An error if the SQL parameter `name` doesn't exist or `value` couldn't be bound.
+	public func bind(value: Database.Value, toParameter name: String) throws {
+		try bind(value: value, toParameter: indexOfParameter(named: name))
+	}
+}
+
+extension Database.Statement {
+	/// Returns the value of the column at `index` for each row in the result set.
+	///
+	/// - note: Column indexes are 0-based.  The leftmost column in a row has index 0.
+	///
+	/// - requires: `index >= 0`
+	/// - requires: `index < self.columnCount`
+	///
+	/// - parameter index: The index of the desired column.
+	///
+	/// - throws: An error if `index` is out of bounds.
+	public func column(_ index: Int) throws -> [Database.Value] {
+		var values = [Database.Value]()
+		try results { row in
+			values.append(try row.value(forColumn: index))
+		}
+		return values
+	}
+
+	/// Returns the value of the column with `name` for each row in the result set.
+	///
+	/// - parameter name: The name of the desired column.
+	///
+	/// - throws: An error if the column `name` doesn't exist.
+	public func column(_ name: String) throws -> [Database.Value] {
+		let index = try index(ofColumn: name)
+		var values = [Database.Value]()
+		try results { row in
+			values.append(try row.value(forColumn: index))
+		}
+		return values
+	}
+
+	/// Returns the values of the columns at `indexes` for each row in the result set.
+	///
+	/// - note: Column indexes are 0-based.  The leftmost column in a row has index 0.
+	///
+	/// - requires: `indexes.min() >= 0`
+	/// - requires: `indexes.max() < self.columnCount`
+	///
+	/// - parameter indexes: The indexes of the desired columns.
+	///
+	/// - throws: An error if any element of `indexes` is out of bounds.
+	public func columns<S: Collection>(_ indexes: S) throws -> [[Database.Value]] where S.Element == Int {
+		var values = [[Database.Value]](repeating: [], count: indexes.count)
+		try results { row in
+			for (n, x) in indexes.enumerated() {
+				values[n].append(try row.value(forColumn: x))
+			}
+		}
+		return values
+	}
+}
--- Sources/Pipeline/Value.swift
+++ Sources/Pipeline/Value.swift
@@ -18,202 +18,10 @@ extension Database {
 		case real(Double)
 		/// A text value.
 		case text(String)
-		/// A blob (untyped bytes) value.
+		/// A BLOB (untyped bytes) value.
 		case blob(Data)
 		/// A null value.
 		case null
-	}
-}
-
-extension Database.Row {
-	/// Returns the value of the column at `index`.
-	///
-	/// - note: Column indexes are 0-based.  The leftmost column in a row has index 0.
-	///
-	/// - parameter index: The index of the desired column.
-	///
-	/// - throws: An error if `index` is out of bounds.
-	///
-	/// - returns: The column's value.
-	public func value(forColumn index: Int) throws -> Database.Value {
-		guard index >= 0, index < sqlite3_data_count(statement.preparedStatement) else {
-			throw Database.Error(message: "Column index \(index) out of bounds")
-		}
-		let type = sqlite3_column_type(statement.preparedStatement, Int32(index))
-		guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-			throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-		}
-		switch type {
-		case SQLITE_INTEGER:
-			let i = sqlite3_column_int64(statement.preparedStatement, Int32(index))
-			guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-			}
-			return .integer(i)
-		case SQLITE_FLOAT:
-			let r = sqlite3_column_double(statement.preparedStatement, Int32(index))
-			guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-			}
-			return .real(r)
-		case SQLITE_TEXT:
-			let t = String(cString: sqlite3_column_text(statement.preparedStatement, Int32(index)))
-			guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-			}
-			return .text(t)
-		case SQLITE_BLOB:
-			guard let b = sqlite3_column_blob(statement.preparedStatement, Int32(index)) else {
-				guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-					throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-				}
-				return .blob(Data())
-			}
-			let count = Int(sqlite3_column_bytes(statement.preparedStatement, Int32(index)))
-			let data = Data(bytes: b.assumingMemoryBound(to: UInt8.self), count: count)
-			return .blob(data)
-		case SQLITE_NULL:
-			return .null
-		default:
-			fatalError("Unknown SQLite column type \(type) encountered for column \(index)")
-		}
-	}
-
-	/// Returns the value of the column at `index` converted to `type`.
-	///
-	/// - note: Column indexes are 0-based.  The leftmost column in a row has index 0.
-	///
-	/// - parameter index: The index of the desired column.
-	/// - parameter type: The desired datatype.
-	///
-	/// - throws: An error if `index` is out of bounds.
-	///
-	/// - returns: The column's value converted to the desired tyoe.
-	public func value(forColumn index: Int, as type: Database.Datatype) throws -> Database.Value {
-		guard index >= 0, index < sqlite3_data_count(statement.preparedStatement) else {
-			throw Database.Error(message: "Column index \(index) out of bounds")
-		}
-		switch type {
-		case .integer:
-			let i = sqlite3_column_int64(statement.preparedStatement, Int32(index))
-			guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-			}
-			return .integer(i)
-		case .real:
-			let r = sqlite3_column_double(statement.preparedStatement, Int32(index))
-			guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-			}
-			return .real(r)
-		case .text:
-			guard let t = sqlite3_column_text(statement.preparedStatement, Int32(index)) else {
-				guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-					throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-				}
-				// SQL NULL
-				return .text("")
-			}
-			// A String created using String(cString:) from a BLOB containing 0x00 will be shorter than expected
-			// but that behavior seems reasonable since type conversion was explicitly requested
-			return .text(String(cString: t))
-		case .blob:
-			guard let b = sqlite3_column_blob(statement.preparedStatement, Int32(index)) else {
-				guard sqlite3_errcode(statement.database.databaseConnection) == SQLITE_OK else {
-					throw SQLiteError(fromDatabaseConnection: statement.database.databaseConnection)
-				}
-				return .blob(Data())
-			}
-			let count = Int(sqlite3_column_bytes(statement.preparedStatement, Int32(index)))
-			let data = Data(bytes: b.assumingMemoryBound(to: UInt8.self), count: count)
-			return .blob(data)
-		case .null:
-			return .null
-		}
-	}
-}
-
-extension Database.Row {
-	/// Returns the values of all columns in the row.
-	///
-	/// - returns: An array of the row's values.
-	public func values() throws -> [Database.Value] {
-		var values: [Database.Value] = []
-		for i in 0 ..< statement.columnCount {
-			values.append(try value(forColumn: i))
-		}
-		return values
-	}
-
-	/// Returns the names and values of all columns in the row.
-	///
-	/// - warning: This method will fail at runtime if the column names are not unique.
-	///
-	/// - returns: A dictionary of the row's values keyed by column name.
-	public func valueDictionary() throws -> [String: Database.Value] {
-		return try Dictionary(uniqueKeysWithValues: statement.columnNames.enumerated().map({ ($0.element, try value(forColumn: $0.offset)) }))
-	}
-}
-
-extension Database.Row {
-	/// Returns the value of the column at `index`.
-	///
-	/// - note: Column indexes are 0-based.  The leftmost column in a row has index 0.
-	///
-	/// - parameter index: The index of the desired column.
-	///
-	/// - returns: The column's value or `nil` if null, the column doesn't exist, or contains an illegal value.
-	public subscript(forColumn index: Int) -> Database.Value? {
-		return try? value(forColumn: index)
-	}
-}
-
-extension Database.Statement {
-	/// Binds `value` to the SQL parameter at `index`.
-	///
-	/// - note: Parameter indexes are 1-based.  The leftmost parameter in a statement has index 1.
-	///
-	/// - parameter value: The desired value of the SQL parameter.
-	/// - parameter index: The index of the SQL parameter to bind.
-	///
-	/// - throws: An error if `value` couldn't be bound.
-	public func bind(_ value: Database.Value, toParameter index: Int) throws {
-		switch value {
-		case .integer(let i):
-			guard sqlite3_bind_int64(preparedStatement, Int32(index), i) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
-			}
-		case .real(let r):
-			guard sqlite3_bind_double(preparedStatement, Int32(index), r) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
-			}
-		case .text(let t):
-			try t.withCString {
-				guard sqlite3_bind_text(preparedStatement, Int32(index), $0, -1, SQLiteTransientStorage) == SQLITE_OK else {
-					throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
-				}
-			}
-		case .blob(let b):
-			try b.withUnsafeBytes {
-				guard sqlite3_bind_blob(preparedStatement, Int32(index), $0.baseAddress, Int32(b.count), SQLiteTransientStorage) == SQLITE_OK else {
-					throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
-				}
-			}
-		case .null:
-			guard sqlite3_bind_null(preparedStatement, Int32(index)) == SQLITE_OK else {
-				throw SQLiteError(fromDatabaseConnection: database.databaseConnection)
-			}
-		}
-	}
-
-	/// Binds `value` to the SQL parameter `name`.
-	///
-	/// - parameter name: The name of the SQL parameter to bind.
-	/// - parameter value: The desired value of the SQL parameter.
-	///
-	/// - throws: An error if the SQL parameter `name` doesn't exist or `value` couldn't be bound.
-	public func bind(_ value: Database.Value, toParameter name: String) throws {
-		try bind(value, toParameter: indexOfParameter(named: name))
 	}
 }
 
@@ -229,12 +37,22 @@ extension Database.Value: Equatable {
 		case (.blob(let b1), .blob(let b2)):
 			return b1 == b2
 		case (.null, .null):
-			// SQL null compares unequal to everything, including null.
+			// SQL NULL compares unequal to everything, including other NULL values.
 			// Is that really the desired behavior here?
 			return false
 		default:
 			return false
 		}
+	}
+}
+
+extension Database.Value {
+	/// Returns `true` if self is `.null`.
+	public var isNull: Bool {
+		if case .null = self {
+			return true
+		}
+		return false
 	}
 }
 
@@ -277,7 +95,7 @@ extension Database.Value: CustomStringConvertible {
 		case .real(let r):
 			return ".real(\(r))"
 		case .text(let t):
-			return ".text('\(t)')"
+			return ".text(\"\(t)\")"
 		case .blob(let b):
 			return ".blob(\(b))"
 		case .null:
