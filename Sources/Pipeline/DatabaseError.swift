aa4c80d24a0fc9a60bd34fdd8798768e19c919b4
Stephen F. Booth <me@sbooth.org>
2022-07-28 22:23:22 -0500

    Add public initializer

--- Sources/Pipeline/DatabaseError.swift
+++ Sources/Pipeline/DatabaseError.swift
@@ -13,15 +13,23 @@ public struct DatabaseError: Error {
 
 	/// A more detailed description of the error's cause.
 	public let details: String?
+
+	/// Creates an error with the given message and details.
+	///
+	/// - parameter message: A brief message describing the error.
+	/// - parameter details: A description of the error's cause.
+	public init(message: String, details: String?) {
+		self.message = message
+		self.details = details
+	}
 }
 
 extension DatabaseError {
 	/// Creates an error with the given message.
 	///
 	/// - parameter message: A brief message describing the error.
-	init(_ message: String) {
-		self.message = message
-		self.details = nil
+	public init(_ message: String) {
+		self.init(message: message, details: nil)
 	}
 }
 
