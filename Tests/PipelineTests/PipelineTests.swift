199d9e2a8a77d27fa8e5827d995d6da9efc311cd
Jason Jobe <box2019@jasonjobe.com>
2022-08-28 01:25:16 -0400

    Added testJSON to validate JSON text storage

--- Tests/PipelineTests/PipelineTests.swift
+++ Tests/PipelineTests/PipelineTests.swift
@@ -208,6 +208,26 @@ final class PipelineTests: XCTestCase {
 		}
 	}
 
+    // jmj
+    func testJSON() {
+        let connection = try! Connection()
+        let list = [1, 2, 3]
+         
+        try! connection.execute(sql: "create table t1(a);")
+                
+        try! connection.prepare(sql: "insert into t1(a) values (?);").bind(.json(list), toParameter: 1).execute()
+        
+        let b = try! connection.prepare(sql: "select * from t1 limit 1;").step()!.get(.json([Int].self), at: 0)
+        
+        XCTAssertEqual(list, b)
+        
+        let s = try! connection.prepare(sql: "select * from t1;")
+        try! s.results { row in
+            let c = try row.get(.string, at: 0)
+            XCTAssertEqual(c, "[1,2,3]")
+        }
+     }
+
 	func testCodable() {
 		let connection = try! Connection()
 
