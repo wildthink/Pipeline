7b7f61f4f423c8b79fad926dcfb18f8bed488b7b
Jason Jobe <box2019@jasonjobe.com>
2024-10-28 17:21:40 -0400

    CSqlite to 3.47.0

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		// .package(url: /* package url */, from: "1.0.0"),
-		.package(url: "https://github.com/sbooth/CSQLite", from: "3.44.0")
+		.package(url: "https://github.com/sbooth/CSQLite", from: "3.47.0")
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
